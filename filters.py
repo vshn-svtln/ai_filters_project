5579c7bae002a970ca9f4625f0be2f63775a099f
tickflag <tickflag-developer@yandex.ru>
2023-11-01 19:58:01 +0300

    upd filter.py

--- filters.py
+++ filters.py
@@ -53,3 +53,22 @@ class LighteninGeer():
                     self.pic.putpixel((i, j), new_value)
 
         return self.pic.show()
+    
+
+#tickflag
+class Filt:
+    def __init__(self, file_dir, dir_to_save):
+        self.file_dir = file_dir
+        self.dir_to_save = dir_to_save
+    
+
+class GrayFilter(Filt):
+    def __init__(self, file_dir, dir_to_save):
+        super().__init__(file_dir, dir_to_save)
+    
+    def gray_filter(self):
+        image = Image.open(self.file_dir).convert('RGB')
+        gray_image = image.convert('L')
+        gray_image.save(self.dir_to_save)
+        gray_image.show()
+    
